e9c6f68e832b83c051d885cb2f493a5e4de4e4ec
CRISTIAN <agudelozc@gmail.com>
2023-12-19 09:22:28 -0500

    Segundo commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/agudelozc/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnterosa()
+	//variables.MuestroEnterosa()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado, texto)
 }
